8a087fe52d2661bcf2f974d382f508392b31e28b
Tiago265 <futlastf998@gmail.com>
2025-02-08 02:10:24 -0300

    meu primeiro arquivo de react

---
+++ helloReactFofo/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css'
+function Navbar(){
+    return(
+        <div className = "container-navbar">
+            <h1> Seja bem vindo ao meu primeiro site com REACT</h1>
+        </div>
+        
+    )
+}
+
+export default Navbar;
+        
