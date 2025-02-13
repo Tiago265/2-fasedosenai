5a827a25cf6268ed4f9b5a6d597e69de9910cc4e
Tiago265 <tiago_sousa-filho@estudante.sesisenai.org.br>
2025-02-13 19:26:03 -0300

    olá mundo

--- helloReactFofo/src/components/Navbar.jsx
+++ helloReactFofo/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ function Navbar(){
     return(
         <div className = "container-navbar">
             <h1> Seja bem vindo ao meu primeiro site com REACT</h1>
+            <h2>hello world</h2>
         </div>
         
     )
