6d1f277b87d41d72ef53b71c520ff4890179320e
TIAGO BURLAMAQUE SOUSA FILHO <tiago_sousa-filho@estudante.sesisenai.org.br>
2025-04-03 21:54:15 -0300

    hello react fofo update

---
+++ inputs/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+const[inputUsername, setInputUsername] = useState('')
+
+const[inputSenha, setInputSenha] = useState('')
+
+const[erro, setErro] = useState(false)
+
+
+  function efetuarLogin(){
+
+
+
+    if(inputUsername === 'Gill Bates' && inputSenha === '1235'){
+
+      setErro(false)
+
+  }else{
+
+    // alert("Erro #403")
+    setErro(true)
+
+  }
+    
+  }
+
+  return (
+
+    <div className='container-app'>
+
+      <h1>Inputs Controlados</h1>
+
+    {positivo &&
+    <span className='msg-acesso'>Acesso Garantido</span>
+    }
+
+      <label htmlFor="">Username</label>
+
+      <input type="text" value={inputUsername}
+      onChange={(event) => setInputUsername(event.target.value)} />
+
+      <label htmlFor="">Senha</label>
+
+      <input type="password" value={inputSenha}
+      onChange={(event) => setInputSenha(event.target.value)} />
+
+
+    {erro && 
+      <span className='msg-erro'>Credenciais Inválidas.</span>
+    }
+    <button onClick={efetuarLogin}>Login</button>
+
+    </div>
+
+  )
+
+}
+
+export default App
