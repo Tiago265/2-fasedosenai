31d414bba6e90b1db5400faae1bae96018c54356
TIAGO BURLAMAQUE SOUSA FILHO <tiago_sousa-filho@estudante.sesisenai.org.br>
2025-04-22 22:17:05 -0300

    props

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,50 +1,81 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Produto from './components/Produto'
 
 function App() {
   const[produtos, setProdutos] = useState([
-    {
-      modelo: "Melancia",
-      preco: 22
-    },
-    {
-      modelo: "Pneu",
-      preco: 220
-    },
-    {
-      modelo: "Garrafa",
-      preco: 6
-    },
-    {
-      modelo: "Notebook",
-      preco: 6000
-    },
-    {
-      modelo: "Desktop pra jogar lol",
-      preco: 600
-    },
+      // {id:Date.now(),  modelo: "Melancia",preco: 22},
+      // {id:Date.now() + 1,  modelo: "Pneu",  preco: 220},
+      // {id:Date.now() + 2,  modelo: "Garrafa",  preco: 6},
+      // {id:Date.now() + 3,  modelo: "Notebook",  preco: 6000},
+      // {id:Date.now() + 4,  modelo: "Desktop pra jogar lol",  preco: 600},
   ])
+  const[inputModelos, setInputModelos] = useState('')
+  const[inputPreco, setInputPreco] = useState('')
+
+  useEffect(() =>
+  console.log("Carregando dados...")
+,[])
 
   function testar(){
+    // console.log(produtos)
+    let produto = {
+      id:Date.now(),
+      modelo: "Produto Secreto",
+      preco: 10
+    }
 
+    console.log(produtos)
+    
+    setProdutos([ produto, ...produtos]) 
+  }
+  
+  function cadastrar(){
+
+    let produto2 = {
+      id:Date.now(),
+      modelo: inputModelos,
+      preco: Number(inputPreco)
+    }
+    setProdutos([ produto2, ...produtos]) 
   }
 
   return (
     <div className='container-app'>
-        <Produto modelo={"Galay A15"} preco={809}/>
+      <div className="form-produto">
+        <label htmlFor="">Digite o produto:</label>
+        <input type="text" 
+        value={inputModelos}
+        onChange={(event) => setInputModelos(event.target.value)}
+        />
+        <label htmlFor="">Digite o preço:</label>
+        <input type="number"
+        value={inputPreco}
+        onChange={(event) => setInputPreco(event.target.value)}
+        />
+          <button onClick={cadastrar}>Cadastrar</button>
+         
+      </div>
+        {/* <Produto modelo={"Galay A15"} preco={809}/>
         <Produto modelo={`Smart TV 32” Full HD LED TCL`} preco={971.10}/>
         <Produto modelo={produtos[0].modelo} preco={produtos[0].preco}/>
         <Produto modelo={produtos[1].modelo} preco={produtos[1].preco}/>
-        <Produto modelo={produtos[2].modelo} preco={produtos[2].preco}/>
+        <Produto modelo={produtos[2].modelo} preco={produtos[2].preco}/> */}
 
-        <button onClick={testar}>Testar</button>
+        {/* <button onClick={testar}>Testar</button> */}
 
-        {produtos.map((p) => (
+        {/* {produtos.map((p, index) => (
           <Produto key={index}modelo={p.modelo} preco={p.preco} />
+        ))} */}
+            <div className="cards">
+        {produtos.map((p) => (
+          <Produto key={p.id} modelo={p.modelo} preco={p.preco} id={p.id}/>
         ))}
+            </div>
+        
     </div>
   )
 }
+
 
 export default App
--- props/src/components/Produto.jsx
+++ props/src/components/Produto.jsx
@@ -3,8 +3,10 @@ import './Produto.css'
 function Produto(props) {
   return (
     <div className='container-Produto'>
+      {/* {console.log(props)} */}
         <h2>{props.modelo}</h2>
         <p>R${props.preco.toFixed(2).replace(".",",")}</p>
+        <p>{props.id}</p>
     </div>
   )
 }
